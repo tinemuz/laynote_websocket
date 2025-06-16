1a44a62243d68ecfa4917236d2d740f784ef26c5
tine <99731290+tinemuz@users.noreply.github.com>
2025-06-16 23:00:13 +0100

    init

---
+++ src/main/java/com/splicedeck/splicedeck_websocket/SplicedeckWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.splicedeck.splicedeck_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplicedeckWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplicedeckWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splicedeck/splicedeck_websocket/SplicedeckWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splicedeck.splicedeck_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplicedeckWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
